cf8e3d8f10dae4d2e62d415a578512e4733135e4
Anton <skanet007@gmail.com>
2020-12-27 19:54:44 +0300

    pre end?

--- SpaceXWidget/SpaceXWidget.swift
+++ SpaceXWidget/SpaceXWidget.swift
@@ -9,52 +9,63 @@
 import WidgetKit
 import SwiftUI
 
-struct Provider: TimelineProvider {
-    func placeholder(in context: Context) -> SimpleEntry {
-        SimpleEntry(date: Date())
+struct LaunchEntry: TimelineEntry {
+    
+    public let date: Date
+    public let name: String
+    public var image: UIImage = UIImage(systemName: "nosign") ?? UIImage()
+}
+
+struct LaunchProvider: TimelineProvider {
+    
+    let net = SpaceXService()
+    typealias Entry = LaunchEntry
+
+    func placeholder(in context: Context) -> LaunchEntry {
+        return LaunchEntry(date: Date(), name: "no name")
     }
     
-//    func imageMaker(in context: Context) -> Image {
-//
-//    }
-
-    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
-        let entry = SimpleEntry(date: Date())
-        completion(entry)
+    func getSnapshot(in context: Context, completion: @escaping (LaunchEntry) -> Void) {
+        let entry = LaunchEntry(date: Date(), name: "name 1")
+        return completion(entry)
     }
+    
+    func getTimeline(in context: Context, completion: @escaping (Timeline<LaunchEntry>) -> Void) {
+        var entries: [LaunchEntry] = []
 
-    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
-        var entries: [SimpleEntry] = []
-
-        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
+        
         let currentDate = Date()
-        for hourOffset in 0 ..< 5 {
-            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = SimpleEntry(date: entryDate)
-            entries.append(entry)
+
+        net.fetchNextLaunch() { result in
+            switch result {
+            case .success(let data):
+                let entry = LaunchEntry(date: currentDate, name: data.name)
+                entries.append(entry)
+                let timeline = Timeline(entries: entries, policy: .atEnd)
+                return completion(timeline)
+            case .failure:
+                print("0")
+            }
         }
-
-        let timeline = Timeline(entries: entries, policy: .atEnd)
-        completion(timeline)
+        
     }
+    
 }
 
-struct SimpleEntry: TimelineEntry {
-    let date: Date
-}
 
 struct SpaceXWidgetEntryView : View {
-    var entry: Provider.Entry
-    
+    var entry: LaunchProvider.Entry
+        
     var body: some View {
         VStack(alignment: .center) {
-            
-//            Text(entry.date, style: .time)
-//                .foregroundColor(.red)
-       //     Spacer()
-            Text("test")
+            Text("Next launch")
+                .minimumScaleFactor(0.5)
+                .font(.title2)
+                .lineLimit(1)
+            Text(entry.name)
+            Image(uiImage: entry.image)
         }
-       
+        
     }
     
 }
@@ -63,8 +74,9 @@ struct SpaceXWidgetEntryView : View {
 struct SpaceXWidget: Widget {
     let kind: String = "SpaceXWidget"
     
+    
     var body: some WidgetConfiguration {
-        StaticConfiguration(kind: kind, provider: Provider()) { entry in
+        StaticConfiguration(kind: kind, provider: LaunchProvider()) { entry in
             SpaceXWidgetEntryView(entry: entry)
         }
         .configurationDisplayName("Pocket SpaceX Widget")
@@ -75,7 +87,7 @@ struct SpaceXWidget: Widget {
 
 struct SpaceXWidget_Previews: PreviewProvider {
     static var previews: some View {
-        SpaceXWidgetEntryView(entry: SimpleEntry(date: Date()))
+        SpaceXWidgetEntryView(entry: LaunchEntry(date: Date(), name: "test name"))
             .previewContext(WidgetPreviewContext(family: .systemSmall))
     }
 }
